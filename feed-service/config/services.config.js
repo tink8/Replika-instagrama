983970ad6171089e35dc633d94f5e68abb9d3eed
Jovana Markovic <jovanaksenijamarkovic@gmail.com>
2026-03-21 13:11:35 +0100

    Feed service configuration

---
+++ feed-service/config/services.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  socialService: process.env.SOCIAL_SERVICE_URL,
+  postService: process.env.POST_SERVICE_URL,
+  userService: process.env.USER_SERVICE_URL,
+  interactionService: process.env.INTERACTION_SERVICE_URL
+};
